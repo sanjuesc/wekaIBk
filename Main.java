4abd9b6d3f74d7ac70dd16a065efbcf3d1e9a01c
sanjuesc <58093412+sanjuesc@users.noreply.github.com>
2021-02-17 20:12:41 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,6 @@
 package ehu.weka;
 
 
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.Random;
 
 import weka.classifiers.Evaluation;
@@ -17,120 +12,123 @@ import weka.core.Instances;
 import weka.core.ManhattanDistance;
 import weka.core.MinkowskiDistance;
 import weka.core.SelectedTag;
-import weka.core.converters.ConverterUtils.DataSource;
-import weka.filters.Filter;
-import weka.filters.unsupervised.instance.Randomize;
-import weka.filters.unsupervised.instance.RemovePercentage;
+import weka.core.converters.ConverterUtils;
+import weka.core.neighboursearch.LinearNNSearch;
 
 
 
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		
-			Double maxpct = 0.0;
-			int maxi=0;
-			int maxj=0;
-			int maxz=0;
-			DataSource loader = new DataSource(args[0]);
-			Instances data = loader.getDataSet();
-			data.setClassIndex(data.numAttributes()-1);
-			/*Randomize randomize = new Randomize();
-			randomize.setSeed(941999);
-			randomize.setInputFormat(data);
-			Instances randomData = Filter.useFilter(data, randomize);
-			RemovePercentage rmp = new RemovePercentage();
-			rmp.setInputFormat(randomData);
-			rmp.setPercentage(30);
-			Instances train = Filter.useFilter(randomData, rmp);
-			
-			rmp = new RemovePercentage();
-			rmp.setInputFormat(randomData);
-			rmp.setPercentage(30);
-			rmp.setInvertSelection(true);
-			Instances test = Filter.useFilter(randomData, rmp);
-			System.out.println(test.numInstances());
-			System.out.println(train.numInstances());
-			//Datuak
-			train.setClassIndex(train.numAttributes()-1);
-			test.setClassIndex(test.numAttributes()-1);
-			*/
-			System.out.println(data.classAttribute().numValues());
-			FileWriter fw = new FileWriter("errors.txt");
-			SelectedTag sim = new SelectedTag(IBk.WEIGHT_SIMILARITY, IBk.TAGS_WEIGHTING);
-			SelectedTag none = new SelectedTag(IBk.WEIGHT_NONE, IBk.TAGS_WEIGHTING);
-			SelectedTag inverse = new SelectedTag(IBk.WEIGHT_INVERSE, IBk.TAGS_WEIGHTING);
-			ManhattanDistance manDistance = new ManhattanDistance();
-			EuclideanDistance euDistance = new EuclideanDistance();
-			ChebyshevDistance cheDistance = new ChebyshevDistance();
-			FilteredDistance filDistance = new FilteredDistance();
-			MinkowskiDistance minkDistance = new MinkowskiDistance();
+	
+            ConverterUtils.DataSource source = new ConverterUtils.DataSource(args[0]);
+            Instances data = source.getDataSet();
+            data.setClassIndex(data.numAttributes()-1);
 
-			for(int i = 1; i<data.numInstances();i++) { 
-				for(int j = 1; j<6; j++) {
-					IBk ibk = new IBk();
-					ibk.setKNN(i);
-					switch(j) {
-					case 1:
-						ibk.getNearestNeighbourSearchAlgorithm().setDistanceFunction(manDistance);
-						break;
-					case 2:
-						ibk.getNearestNeighbourSearchAlgorithm().setDistanceFunction(euDistance);
-						break;
-					case 3:
-						ibk.getNearestNeighbourSearchAlgorithm().setDistanceFunction(cheDistance);
-						break;
-					case 4: 
-						ibk.getNearestNeighbourSearchAlgorithm().setDistanceFunction(filDistance);
-						break;
-					case 5: 
-						ibk.getNearestNeighbourSearchAlgorithm().setDistanceFunction(minkDistance);
-						break;
-					default:
-			    		break;
-					}
-					for(int z= 1; z<4; z++) {
-						switch(z) {
-						case 1:
-							ibk.setDistanceWeighting(inverse);
-							break;
-						case 2:
-							ibk.setDistanceWeighting(none);
-							break;
-						case 3:
-							ibk.setDistanceWeighting(sim);
-							break;
-						default:
-				    		break;
-						}try {
-							Evaluation eval = new Evaluation(data);
-							eval.crossValidateModel(ibk, data, 10, new Random(1));
-							System.out.println("\nCaso actual i,j,z "+i+","+j+","+z);
-							if(eval.pctCorrect()>maxpct) {
-								maxpct=eval.pctCorrect();
-								maxi=i;
-								maxj=j;
-								maxz=z;
-							}
-							
-						}catch(Exception e) {
-							StringWriter sw = new StringWriter();
-							PrintWriter pw = new PrintWriter(sw);
-							e.printStackTrace(pw);
-							String sStackTrace = sw.toString(); 
-							fw.write("\nCaso actual i,j,z "+i+","+j+","+z);
-							fw.write(sStackTrace);
-						}
+            LinearNNSearch manDistance = new LinearNNSearch();
+            manDistance.setDistanceFunction(new ManhattanDistance());
+            LinearNNSearch euDistance = new LinearNNSearch();
+            euDistance.setDistanceFunction(new EuclideanDistance());
+            LinearNNSearch cheDistance = new LinearNNSearch();
+            cheDistance.setDistanceFunction(new ChebyshevDistance());
+            LinearNNSearch filDistance = new LinearNNSearch();
+            filDistance.setDistanceFunction(new FilteredDistance());
+            LinearNNSearch minkDistance = new LinearNNSearch();
+            MinkowskiDistance minko = new MinkowskiDistance();
+            minko.setOrder(3.0);
+            minkDistance.setDistanceFunction(minko);
 
-					}
+            
+            LinearNNSearch[] distances = new LinearNNSearch[]{manDistance, euDistance, cheDistance, filDistance, minkDistance};
 
-				}
+            SelectedTag[] tags = new SelectedTag[]{new SelectedTag(IBk.WEIGHT_NONE, IBk.TAGS_WEIGHTING), new SelectedTag(IBk.WEIGHT_INVERSE, IBk.TAGS_WEIGHTING), new SelectedTag(IBk.WEIGHT_SIMILARITY, IBk.TAGS_WEIGHTING)};
 
-			}
+            Evaluation eval = null;
+            IBk ibk = new IBk();
+            Double maxf = 0.0;
+            int maxk = 0;
+            Evaluation maxeval = new Evaluation(data);
+            LinearNNSearch maxd;
+            SelectedTag maxw= null;
+            int index = 0;
+            int maxindex = 0;
+            for (int k = 1; k <= data.numInstances()*0.9; k++) {
+                ibk.setKNN(k);
+                for (LinearNNSearch d : distances) {
+                    if(index==distances.length){
+                        index = 0;
+                    }
+                    ibk.setNearestNeighbourSearchAlgorithm(d);
+                	int pisua = 0;
+                    for (SelectedTag w : tags) {
+                    	try {
+                    		eval = new Evaluation(data);
+                    		ibk.setDistanceWeighting(w);
+                    		eval.crossValidateModel(ibk, data, 10, new Random());
+                    		Double fmeasure = eval.weightedFMeasure();
+                    		System.out.println("k = " + k + " distantzia = " + printeatudistantzia(index) + " eta pisua = " + pisua(pisua));
+                    		pisua++;
+                    		if (maxf < fmeasure) {
+                    			maxf = fmeasure;
+                    			maxk = k;
+                    			maxd = d;
+                    			maxw = w;
+                    			maxindex = index;
+                    			maxeval=eval;
+                    		}
+                    		
+                    	}catch(Exception e) {
+                    		System.out.println("k = " + k + " distantzia = " + printeatudistantzia(index) + " eta pisua = " + w + " ERROREA");
+                    	}
+               
+                    }
+                    index ++;
+                }
+            }
 
-			fw.close();
-			System.out.println("Casu hoberenean pctCorrect " + maxpct + " izan da");
-			System.out.println("i = " + maxi + ", j = "+ maxj + ", z = "+ maxz);
+            System.out.println("EMAITZAK: k-ren balio optimoa = " + maxk + " da, " + printeatudistantzia(maxindex)+ " distantziarekin eta " + maxw + " pisuarekin non fmeasure = " + maxf + " den.");
+            System.out.println(maxeval.toMatrixString());
+    }
 
+
+
+	private static String pisua(int pisua) {
+		switch(pisua) {
+		case 0:
+			return "None";
+		case 1: 
+			return "Inverse";
+		case 2:
+			return "Similarity";
+		}
+		return null;
+	}
+
+
+
+	private static String printeatudistantzia(int i) {
+        String emaitza ="";
+        switch (i){
+            case 0:
+                emaitza = "Manhattan";
+                break;
+            case 1:
+                emaitza = "Euclidean";
+                break;
+            case 2:
+                emaitza = "Chevishev";
+                break;
+            case 3:
+                emaitza = "Filtered";
+                break;
+            case 4:
+                emaitza = "Minkowski";
+                break;
+            default:
+                emaitza = "Manhattan";
+                break;
+        }
+        return emaitza;
+    
 	}
 }
